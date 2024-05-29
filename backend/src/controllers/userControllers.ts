729237f5607ed0763182fe92618502d7ec37f2a8
Laurent Binder <laurentbinder@gmail.com>
2024-05-29 16:17:19 +0200

    add userSignup

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcrypt';
 import { NextFunction, Request, Response } from 'express';
 import User from '../models/User.js';
 
@@ -7,11 +8,33 @@ export const getAllUsers = async (
   next: NextFunction
 ) => {
   try {
-    console.log('you are here little user! ');
     const users = await User.find();
     return res.status(200).json({ message: 'OK', users });
   } catch (error) {
     console.log('🙀 ~ getAllUsers ~ error:', error);
-    return res.status(500).json({ message: 'OK', cause: error });
+    return res
+      .status(500)
+      .json({ message: "can't get all users", cause: error });
+  }
+};
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+    newUser.save();
+    return res.status(200).json({
+      message: 'A new user has been created',
+      id: newUser._id.toString(),
+    });
+  } catch (error) {
+    console.log('🙀 ~ getAllUsers ~ error:', error);
+    return res
+      .status(500)
+      .json({ message: 'Error during signup', cause: error });
   }
 };
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import { getAllUsers } from '../controllers/userControllers.js';
+import { getAllUsers, userSignup } from '../controllers/userControllers.js';
 
 const userRoutes = Router();
 
-userRoutes.get('/test', function (req, res) {
-  console.log('🚀 ~ res:', res);
-  res.sendFile('test.html', { root: __dirname });
-});
 userRoutes.get('/', getAllUsers);
+userRoutes.post('/signup', userSignup);
 
 export default userRoutes;
