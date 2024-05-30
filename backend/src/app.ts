c70f3bbe0e1171a8d2085f8a68275357989cd0f3
Laurent Binder <laurentbinder@gmail.com>
2024-05-30 16:34:27 +0200

    add token auth and cookie parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
@@ -8,6 +9,7 @@ const app = express();
 
 // Middleware pour analyser les corps de requête JSON
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // remove it in production
 app.use(morgan('dev'));
--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,6 +1,8 @@
 import { compare, hash } from 'bcrypt';
 import { NextFunction, Request, Response } from 'express';
 import User from '../models/User.js';
+import { COOKIE_NAME } from '../utils/constants.js';
+import { createToken } from '../utils/tokenManager.js';
 
 export const getAllUsers = async (
   req: Request,
@@ -24,12 +26,33 @@ export const userSignup = async (req: Request, res: Response) => {
     const existingUser = User.findOne({ email });
     if (existingUser) return res.status(401).send('User already registered');
     const hashedPassword = await hash(password, 10);
-    const newUser = new User({ name, email, password: hashedPassword });
-    newUser.save();
+    const user = new User({ name, email, password: hashedPassword });
+    user.save();
+
+    // create token and store cookie
+
+    res.clearCookie(COOKIE_NAME, {
+      path: '/',
+      domain: 'localhost',
+      httpOnly: true,
+      signed: true,
+    });
+
+    const token = createToken(user._id.toString(), user.email, '7d');
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: '/',
+      domain: 'localhost',
+      expires,
+      httpOnly: true,
+      signed: true,
+      // secure: true, // Make sure you use HTTPS in production
+    });
 
     return res.status(200).json({
       message: 'A new user has been created',
-      id: newUser._id.toString(),
+      id: user._id.toString(),
     });
   } catch (error) {
     console.log('🙀 ~ getAllUsers ~ error:', error);
@@ -46,6 +69,27 @@ export const userLogin = async (req: Request, res: Response) => {
     if (!user) return res.status(401).send('User not registered');
     const isPasswordCorrect = await compare(password, user.password);
     if (!isPasswordCorrect) return res.status(403).send('Incorrect password');
+
+    // create token and store cookie
+
+    res.clearCookie(COOKIE_NAME, {
+      path: '/',
+      domain: 'localhost',
+      httpOnly: true,
+      signed: true,
+    });
+
+    const token = createToken(user._id.toString(), user.email, '7d');
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: '/',
+      domain: 'localhost',
+      expires,
+      httpOnly: true,
+      signed: true,
+      // secure: true, // Make sure you use HTTPS in production
+    });
 
     return res.status(200).json({
       message: 'Successfully logged',
