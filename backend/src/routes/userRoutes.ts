1e6f0993959b855ed25dae6def60027471179d97
Laurent Binder <laurentbinder@gmail.com>
2024-06-11 20:04:48 +0200

    add userLogout

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   getAllUsers,
   userLogin,
+  userLogout,
   userSignup,
   verifyUser,
 } from '../controllers/userControllers.js';
@@ -18,6 +19,6 @@ userRoutes.get('/', getAllUsers);
 userRoutes.post('/signup', validate(signupValidator), userSignup);
 userRoutes.post('/login', validate(loginValidation), userLogin);
 userRoutes.get('/auth-status', verifyToken, verifyUser);
-userRoutes.get('/logout', verifyToken, verifyUser);
+userRoutes.get('/logout', verifyToken, userLogout);
 
 export default userRoutes;
